e7304c494167f7a15a1a53dd9dcf0fab442ba46b
Matt Hunt <wmatthunt@gmail.com>
2020-09-13 17:33:36 -0400

    added first round of questions

--- index.js
+++ index.js
@@ -15,3 +15,61 @@ var connection = mysql.createConnection({
   password: "Thunder1",
   database: "employee_trackerDB"
 });
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Add departments, roles, or employees",
+        "View departments, roles, or employees",
+        "Update employee roles"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Add departments, roles, or employees":
+        add();
+        break;
+
+      case "View departments, roles, or employees":
+        view();
+        break;
+
+      case "Update employee roles":
+        update();
+        break;
+      }
+    });
+}
+function add(){
+  inquirer
+    .prompt({
+      name: "new",
+      type: "input",
+      message: "Would you like to add a department, role, or employee?"
+    })
+}
+function view(){
+  inquirer
+    .prompt({
+      name: "search",
+      type: "input",
+      message: "Would you like to view a department, role, or employee?"
+    })
+}
+function update(){
+  inquirer
+    .prompt({
+      name: "promote",
+      type: "input",
+      message: "Who would you like to promote?"
+    })
+}
